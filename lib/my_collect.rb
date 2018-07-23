fc34738f88bd8cd84305baf0d6338e7fc0876b29
angelica winkler <caw400@nyu.edu>
2018-07-23 00:56:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,8 @@ def my_collect (array)
   newArray = []
 
   while i < array.length
-    yield array[i]
+    newArray << yield array[i]
     i = i + 1
-    newArray.push(my_collect(array[i]))
   end
 
   newArray
